0f4c9a499823832d8120a62ef8f7d15937a5159a
Marbax <40849771+Marbax@users.noreply.github.com>
2021-02-03 12:59:10 +0200

    made menu from map

--- src/components/MenuNavbar/Menu/index.js
+++ src/components/MenuNavbar/Menu/index.js
@@ -4,31 +4,25 @@ import cn from 'classnames'
 import style from './style.module.css'
 
 const Menu = ({ menuActive, handleLinkClick }) => {
+    const PAGES = [
+        { path: '/', name: 'HOME' },
+        { path: '/game', name: 'GAME' },
+        { path: '/about', name: 'ABOUT' },
+        { path: '/contact', name: 'CONTACT' },
+    ]
+
     return (
         <div className={cn(style['menu-container'], menuActive ? style.active : style.deactive)}>
             <div className={style.overlay} />
             <div className={style['menu-items']}>
                 <ul>
-                    <li>
-                        <NavLink exact to='/' activeClassName={style.active} className={style['nav-link']} onClick={handleLinkClick}>
-                            HOME
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/game' activeClassName={style.active} className={style['nav-link']} onClick={handleLinkClick}>
-                            GAME
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/about' activeClassName={style.active} className={style['nav-link']} onClick={handleLinkClick}>
-                            ABOUT
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to='/contact' activeClassName={style.active} className={style['nav-link']} onClick={handleLinkClick}>
-                            CONTACT
-                        </NavLink>
-                    </li>
+                    {PAGES.map((i) => (
+                        <li key={i.name}>
+                            <NavLink exact to={i.path} activeClassName={style.active} className={style['nav-link']} onClick={handleLinkClick}>
+                                {i.name}
+                            </NavLink>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
