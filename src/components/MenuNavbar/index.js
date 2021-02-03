72e910478a22222e8242d27b1cc82025f2eb9c8d
Marbax <40849771+Marbax@users.noreply.github.com>
2021-02-03 12:42:20 +0200

    changed state value name

--- src/components/MenuNavbar/index.js
+++ src/components/MenuNavbar/index.js
@@ -4,14 +4,14 @@ import Menu from './Menu'
 import Navbar from './Navbar'
 
 const MenuNavbar = () => {
-    const [menuActive, setMenuActive] = useState(false)
+    const [isMenuActive, setMenuActive] = useState(false)
 
     const handleBurgerClick = () => setMenuActive((s) => !s)
 
     return (
         <>
-            <Navbar menuActive={menuActive} handleBurgerClick={handleBurgerClick} />
-            <Menu menuActive={menuActive} handleLinkClick={handleBurgerClick} />
+            <Navbar menuActive={isMenuActive} handleBurgerClick={handleBurgerClick} />
+            <Menu menuActive={isMenuActive} handleLinkClick={handleBurgerClick} />
         </>
     )
 }
