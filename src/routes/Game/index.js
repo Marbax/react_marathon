4a91573cde303494725776973d5e47cacec5dcba
Marbax <40849771+Marbax@users.noreply.github.com>
2021-02-05 20:23:48 +0200

    revorked pokemos to work with firebase

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,21 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PokemonCard from '../../components/PokemonCard'
 import Layout from '../../components/Layout'
+import database from '../../services/firebase'
+
+import LayoutBg from '../../assets/bgSleepingPika.jpg'
 
 import style from './style.module.css'
 
-const GamePage = ({ pokemons }) => {
-    const [pokemonsArr, SetPokemonsArr] = useState(pokemons)
+const GamePage = () => {
+    const [pokemons, SetPokemons] = useState({})
 
     const handleCardClick = (pokemonId) => {
-        SetPokemonsArr((state) => state.map((item) => (item.id === pokemonId ? { ...item, isActive: !item.isActive } : item)))
+        SetPokemons((prevState) => {
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = { ...item[1] }
+                if (pokemon.id === pokemonId) {
+                    pokemon.active = !pokemon.active
+                    database.ref('pokemons/' + item[0]).set({
+                        ...pokemon,
+                    })
+                }
+
+                acc[item[0]] = pokemon
+
+                return acc
+            }, {})
+        })
     }
+
+    const handleAddPokeClick = () => {
+        const newPostKey = database.ref().child('pokemons').push().key
+        const newPoke = Object.entries(pokemons)[0][1]
+        //TODO: remove
+        //TEMP id gen
+        newPoke.id = Object.entries(pokemons).reduce((max, item) => (item[1].id > max ? item[1].id : max), Object.entries(pokemons)[0][1].id) + 1
+        newPoke.active = false
+        //TEMP
+        database.ref('pokemons/' + newPostKey).set(newPoke)
+
+        getAll()
+    }
+
+    const getAll = () => {
+        database.ref('pokemons').once('value', (snapshot) => {
+            SetPokemons(snapshot.val())
+        })
+    }
+
+    useEffect(() => {
+        getAll()
+    }, [])
 
     return (
         <>
-            <Layout colorBg={'purple'}>
+            <Layout urlBg={LayoutBg}>
+                <button onClick={handleAddPokeClick}>Add pokemon</button>
                 <div className={style['card-container']}>
-                    {pokemonsArr && pokemonsArr.map((item) => <PokemonCard key={`${item.id}`} {...item} onClick={handleCardClick} isActive={item.isActive} />)}
+                    {Object.entries(pokemons).map(([key, { id, type, img, name, values, bgImg, active }]) => (
+                        <PokemonCard key={key} id={id} type={type} img={img} name={name} values={values} bgImg={bgImg} isActive={active} onClick={handleCardClick} />
+                    ))}
                 </div>
             </Layout>
         </>
