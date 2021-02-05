81f7c005a629e060e4c9713dad47ed325520ad9c
Marbax <40849771+Marbax@users.noreply.github.com>
2021-02-05 20:25:22 +0200

    fixed simple console errors

--- src/components/MenuNavbar/Navbar/index.js
+++ src/components/MenuNavbar/Navbar/index.js
@@ -4,7 +4,7 @@ import style from './style.module.css'
 
 const Navbar = ({ handleBurgerClick, isMenuActive, isBgActive }) => {
     return (
-        <nav id={style.navbar} className={isBgActive && style['bg-active']}>
+        <nav id={style.navbar} className={isBgActive ? style['bg-active'] : ''}>
             <div className={style['nav-wrapper']}>
                 <p className={style.brand}>Mbx</p>
                 <span onClick={handleBurgerClick} className={cn(style['menu-button'], isMenuActive && style.active)}>
