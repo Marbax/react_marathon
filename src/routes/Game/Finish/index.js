63f8c0c098f26bb846a2392f7df02787f8320b96
Marbax <40849771+Marbax@users.noreply.github.com>
2021-02-08 22:54:49 +0200

    started game finish page

---
+++ src/routes/Game/Finish/index.js
@@ -0,0 +1,12 @@
+import style from './style.module.css'
+
+const FinishPage = () => {
+    return (
+        <>
+            <h1>Finish</h1>
+            <div></div>
+        </>
+    )
+}
+
+export default FinishPage
