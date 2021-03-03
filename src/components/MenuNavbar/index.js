e61fd22981c075947356f20b7a9466b6fb4f1f69
Marbax <Marbax117@gmail.com>
2021-03-03 18:24:54 +0200

    made basic authentication actions

--- src/components/MenuNavbar/index.js
+++ src/components/MenuNavbar/index.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { firebaseSignInUrl, firebaseSignUpUrl } from '../../services/firebase'
 import LoginForm from '../LoginForm'
 import Modal from '../Modal'
+import { NotificationManager } from 'react-notifications'
 
 import Menu from './Menu'
 import Navbar from './Navbar'
@@ -8,11 +10,38 @@ import Navbar from './Navbar'
 const MenuNavbar = ({ bgActive }) => {
     const [isMenuActive, setMenuActive] = useState(null)
     const [isModalOpen, setModalOpen] = useState(null)
+    //state to display login or register form and to proccess submit differently
+    const [isLoginForm, setLoginForm] = useState(true)
 
     const handleBurgerClick = () => setMenuActive((prevState) => !prevState)
     const handleLoginClick = () => setModalOpen((prevState) => !prevState)
     const handleModalCloseClick = () => setModalOpen(false)
-    const handleSubmitLoginForm = (data) => console.log(data)
+    const handleSubmitLoginForm = async ({ email, password }) => {
+        const requestOptions = {
+            method: 'POST',
+            body: JSON.stringify({
+                email,
+                password,
+                returnSecureToken: true,
+            }),
+        }
+        const resp = await fetch(
+            isLoginForm ? firebaseSignInUrl : firebaseSignUpUrl,
+            requestOptions
+        )
+        const data = await resp.json()
+
+        if (data.hasOwnProperty('error')) {
+            NotificationManager.error(data?.error?.message, 'Oops...')
+        } else {
+            NotificationManager.success(
+                `${data?.email} successfully ${isLoginForm ? 'logined' : 'registered'}.`,
+                'Gotcha'
+            )
+            console.log(data)
+        }
+    }
+    const handleChangeFormState = () => setLoginForm((prevState) => !prevState)
 
     return (
         <>
@@ -23,8 +52,15 @@ const MenuNavbar = ({ bgActive }) => {
                 onClickLogin={handleLoginClick}
             />
             <Menu isMenuActive={isMenuActive} handleLinkClick={handleBurgerClick} />
-            <Modal clickCloseModal={handleModalCloseClick} isOpen={isModalOpen} title='Login...'>
-                <LoginForm onSubmit={handleSubmitLoginForm} isActive={isModalOpen}></LoginForm>
+            <Modal
+                clickCloseModal={handleModalCloseClick}
+                isOpen={isModalOpen}
+                title={isLoginForm ? 'Login...' : 'Register...'}>
+                <LoginForm
+                    login={isLoginForm}
+                    onChangeFormState={handleChangeFormState}
+                    onSubmit={handleSubmitLoginForm}
+                    isActive={isModalOpen}></LoginForm>
             </Modal>
         </>
     )
